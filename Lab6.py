2021ea5194b660d1125beecf2fb83d6155b0fe14
David Vance <dvance4@mail.pima.edu>
2024-10-02 11:47:22 -0700

    Update Lab6.py
    Updated program to represent the three modules required.  Never code at 1am.

--- Lab6.py
+++ Lab6.py
@@ -6,16 +6,19 @@
 """
 Hot Dog Cookout Calculator
 
-Assume that hot dogs come in packages of 10, and hot dog buns come in packages of 8. 
-Design a modular program that calculates the number of packages of hot dogs and the number of 
-packages of hot dog buns needed for a cookout, with the minimum amount of leftovers. 
-The program should ask the user for the number of people attending the cookout, and the 
-number of hot dogs each person will be given. The program should display the following:
+Assume that hot dogs come in packages of 10, and hot dog buns come in packages
+of 8.  Design a modular program that calculates the number of packages of hot 
+dogs and the number of packages of hot dog buns needed for a cookout, with the
+minimum amount of leftovers.
 
-1. The minimum number of packages of hot dogs required.
-2. The minimum number of packages of buns required
-3. The number of hot dogs that will be left over
-4. The number of buns that will be left over
+The program should ask the user for the number of people attending the cookout,
+and the number of hot dogs each person will be given. 
+
+The program should display the following:
+    1. The minimum number of packages of hot dogs required.
+    2. The minimum number of packages of buns required
+    3. The number of hot dogs that will be left over
+    4. The number of buns that will be left over
 """
 
 # INITIALIZING
@@ -23,71 +26,81 @@ number of hot dogs each person will be given. The program should display the fol
 
 import math  # This is necessary to call the math.ceil function below
 
-def get_total_hotdogs():
-    """This module gets the number of people attending the cookout and the 
-    number of hot dogs each person will be given, and stores the product in
-    the total_hotdogs reference variable."""
+
+def main():
+    """Get the number of hotdogs needed, then calculate values for leftover and
+    minimum needed hotdogs, then calculate values for leftover and minimum 
+
     
-    # Get the number of people attending the cookout
-    people = int(input('Enter the number of people attending the cookout:')) 
+            needed buns"""
 
-    # Get the number of hot dogs each person will be given
-    hot_dogs_per = int(input('Enter the number of hot dogs for each person: '))
+    # Get the total number of hot dogs needed
+    total = get_total_hot_dogs()
 
-    # Calculate the total number of hotdogs needed
-    total_hotdogs = people * hot_dogs_per
-    return total_hotdogs
+    # Named Constants
+    dogs_per_package = 10  # The number of hot dogs in a package
+    buns_per_package = 8   # The number of buns in a package
 
+    # Local variables
+    # I don't believe this is necessary; I've run the program without just
+    # fine. However, I am including it because it was in the assignment's
+    # pseudocode.
+    dogs_left = 0
+    buns_left = 0
+    min_dogs = 0
+    min_buns = 0
 
-def show_results(dogs_left, min_dogs, buns_left, min_buns):
-    """This module accepts the total number of hot dogs as an argument and calculates the minimum packages of hot
-    dogs and hot dog buns, the left over hot dogs and hot dog buns, and displays the results."""
+    # Calculate for leftover and minimum hot dogs
+    dogs_left = (dogs_per_package - total % dogs_per_package) % dogs_per_package
+    min_dogs = math.ceil(total / dogs_per_package) 
 
-    # Display the minimum packages of hot dogs needed
-    print('Minimum packages of hot dogs needed: ', min_dogs)
+    # Calculate for leftover and minimum buns
+    buns_left = (buns_per_package - total % buns_per_package) % buns_per_package
+    min_buns = math.ceil(total / buns_per_package) 
 
-    # Display the minimum packages of buns needed
-    print('Minimum packages of hot dog buns needed: ', min_buns)
-
-    # Display the number of hot dogs left over
-    print('Hot dogs left over: ', dogs_left)
-
-    # Display the number of hot dog buns left over
-    print('Hot dog buns left over: ', buns_left)
+    # Display results
+    show_results(dogs_left, min_dogs, buns_left, min_buns)
     return
 
 
-# Set the number of hotdogs and buns per package
-dogs_per_package = 10
-buns_per_package = 8
+def get_total_hot_dogs():
+    """This module gets the number of people attending the cookout, the 
+    number of hot dogs each person will be given, calculates the total hot
+    dogs needed, and stores the result in the total_hot_dogs reference
+    variable."""
+    
+    # Get the number of people attending and hotdogs per
+    people = int(input('Enter the number of people attending the cookout:')) 
+    hot_dogs_per = int(input('Enter the number of hot dogs for each person: '))
+
+    # Calculate the total number of hotdogs needed
+    total_hot_dogs = people * hot_dogs_per
+    return total_hot_dogs
+
+
+def show_results(dogs_left, min_dogs, buns_left, min_buns):
+    """This module receives the total number of hot dogs
+    as an argument and calculates the minimum packages of hot
+    dogs and hot dog buns, the left over hot dogs and hot dog
+    buns, and displays the results."""
+    # This description from the pseudocode is incorrect.  This module
+    # accepts the values calculated in main() and displays the result.
+
+    # Display the minimum packages of hotdogs and buns needed
+    print('Minimum packages of hot dogs needed: ', min_dogs)
+    print('Minimum packages of hot dog buns needed: ', min_buns)
+
+    # Display the number of hot dogs and buns left over
+    print('Hot dogs left over: ', dogs_left)
+    print('Hot dog buns left over: ', buns_left)
+    return
 
 # PROCESSING
-# Get the number of hotdogs needed, then calculate values for leftover and 
-# minimum needed hotdogs, then calculate values for leftover and minimum
-# needed buns
+# With all libraries imported, functions defined and variables assigned it is
+# time to actually initialize the program.
 
-# Get the total number of hot dogs needed
-total = get_total_hotdogs()
-
-# Calculate the number of leftover hot dogs
-dogs_left = (dogs_per_package - total % dogs_per_package) % dogs_per_package
-
-# Calculate the minimum number of packages of hot dogs
-min_dogs = math.ceil(total / dogs_per_package) 
-
-# Calculate the number of leftover hot dog buns
-buns_left = (buns_per_package - total % buns_per_package) % buns_per_package
-
-#Calculate the minimum number of packages of hot dogs buns
-min_buns = math.ceil(total / buns_per_package) 
+main() 
 
 
 # TERMINATION
-# Display the results of hotdogs left, minimum number of hotdogs needed,
-# and buns left and minimum buns needed
-
-show_results(dogs_left, min_dogs, buns_left, min_buns)
-
-
-
-
+# The outputs are actually given in the show_results module.
